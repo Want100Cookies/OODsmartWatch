582cd975f2a825cc35b1c4533fd35e576a2a4e32
JennoVink <jenno-vink@hotmail.com>
2016-03-22 15:11:43 +0100

    Klein opzetje gemaakt voor het kliksysteem. Bug: wanneer je lang een button inhoud wordt dit keer op keer gemeten als een long button press. Potential fix: elk buttonDown event vervangen door een click event.

--- WatchOS/WatchOS/Form1.Designer.cs
+++ WatchOS/WatchOS/Form1.Designer.cs
@@ -28,55 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button1Timer = new System.Windows.Forms.Timer(this.components);
+            this.button2Timer = new System.Windows.Forms.Timer(this.components);
+            this.doubleClickTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(134, 12);
+            this.panel1.Location = new System.Drawing.Point(119, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(671, 638);
+            this.panel1.Size = new System.Drawing.Size(596, 510);
             this.panel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 714);
+            this.button1.Location = new System.Drawing.Point(119, 571);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 37);
+            this.button1.Size = new System.Drawing.Size(82, 30);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "button 1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button1Down);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1Up);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(404, 714);
+            this.button2.Location = new System.Drawing.Point(359, 571);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 37);
+            this.button2.Size = new System.Drawing.Size(82, 30);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "button 2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button2Down);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button2Up);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(713, 714);
+            this.button3.Location = new System.Drawing.Point(611, 571);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(92, 37);
+            this.button3.Size = new System.Drawing.Size(104, 30);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "button 1 + 2 together";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1Timer
+            // 
+            this.button1Timer.Interval = 200;
+            this.button1Timer.Tick += new System.EventHandler(this.button1Timer_Tick);
+            // 
+            // button2Timer
+            // 
+            this.button2Timer.Interval = 200;
+            this.button2Timer.Tick += new System.EventHandler(this.button2Timer_Tick);
+            // 
+            // doubleClickTimer
+            // 
+            this.doubleClickTimer.Interval = 180;
+            this.doubleClickTimer.Tick += new System.EventHandler(this.doubleClickTimer_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 795);
+            this.ClientSize = new System.Drawing.Size(844, 636);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -89,6 +117,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer button1Timer;
+        private System.Windows.Forms.Timer button2Timer;
+        private System.Windows.Forms.Timer doubleClickTimer;
     }
 }
 
--- WatchOS/WatchOS/Form1.cs
+++ WatchOS/WatchOS/Form1.cs
@@ -16,5 +16,79 @@ namespace WatchOS
         {
             InitializeComponent();
         }
+
+        private void Button1Down(object sender, MouseEventArgs e)
+        {
+            button1Timer.Enabled = true;
+            if (doubleClickTimer.Enabled)
+            {
+                //timer staat al aan: doubleclicked!
+                 System.Diagnostics.Debug.WriteLine("double clicked button 1");
+            }
+              doubleClickTimer.Enabled = true;
+        }
+
+        private void Button1Up(object sender, MouseEventArgs e)
+        {
+            button1Timer.Enabled = false;
+        }
+
+        private void Button2Down(object sender, MouseEventArgs e)
+        {
+            button2Timer.Enabled = true;
+            
+            if (doubleClickTimer.Enabled)
+            {
+                //timer staat al aan: doubleclicked!
+                System.Diagnostics.Debug.WriteLine("double clicked btn 2");
+            }
+            doubleClickTimer.Enabled = true;
+        }
+
+        private void Button2Up(object sender, MouseEventArgs e)
+        {
+            button2Timer.Enabled = false;
+        }
+
+        private void changeWatchMode()
+        {
+            
+        }
+
+        private void buttonShort(int buttonNo)
+        {
+
+        }
+
+        private void buttonLong(int buttonNo)
+        {
+            
+        }
+
+        private void buttonTwice(int buttonNo)
+        {
+            
+        }
+
+        private void button1Timer_Tick(object sender, EventArgs e)
+        {
+            button1Timer.Enabled = false;
+             System.Diagnostics.Debug.WriteLine("button 1 long clicked!");
+            //buttonLong(1);
+        }
+
+        private void button2Timer_Tick(object sender, EventArgs e)
+        {
+            button1Timer.Enabled = false;
+            System.Diagnostics.Debug.WriteLine("button 2 long clicked!");
+            //buttonLong(1);
+        }
+
+        private void doubleClickTimer_Tick(object sender, EventArgs e)
+        {
+            doubleClickTimer.Enabled = false;
+
+           //  System.Diagnostics.Debug.WriteLine("timer reset");
+        }
     }
 }
