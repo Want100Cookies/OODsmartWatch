406888218aa5d39f9fdbbc39ceb996b92c5c1ad1
Lesley Jordan van Oostenrijk <Lesley Jordan van Oostenrijk>
2016-03-30 22:04:38 +0200

    imgur poep

--- WatchOS/Imgur.Designer.cs
+++ WatchOS/Imgur.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.imgurScreen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgurScreen)).BeginInit();
             this.SuspendLayout();
+            // 
+            // imgurScreen
+            // 
+            this.imgurScreen.Location = new System.Drawing.Point(-4, -2);
+            this.imgurScreen.Name = "imgurScreen";
+            this.imgurScreen.Size = new System.Drawing.Size(378, 307);
+            this.imgurScreen.TabIndex = 0;
+            this.imgurScreen.TabStop = false;
             // 
             // Imgur
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(371, 305);
+            this.Controls.Add(this.imgurScreen);
             this.Location = new System.Drawing.Point(300, 50);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Imgur";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "Imgur";
+            this.Text = "L";
+            this.Load += new System.EventHandler(this.Imgur_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgurScreen)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imgurScreen;
     }
 }
--- WatchOS/Imgur.cs
+++ WatchOS/Imgur.cs
@@ -7,12 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Newtonsoft.Json;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
 
 namespace WatchOS
 {
     public partial class Imgur : Form
     {
+        new LinkedList LinkedList<Post> posts 
 
+        public void loadPosts()
+        {
+            var client = new ImgurClient("749421e4eed8f22", "87f3637908d8c0fa86c567db9e447ad166a9abdb");
+            var endpoint = new GalleryEndpoint(client);
+            var task = endpoint.GetGalleryAsync();
+            Task.WaitAll(task);
+            var images = task.Result;
+            Console.WriteLine("is geladen");
+            foreach (var image in images)
+            {
+                imgurScreen.Image = image.
+                Console.WriteLine(image.Score);
+            }
+        }
+
+        private void Imgur_Load(object sender, EventArgs e)
+        {
+            loadPosts();
+        }
+
+        public Imgur()
+        {
+            loadPosts();
+        }
     }
 }
