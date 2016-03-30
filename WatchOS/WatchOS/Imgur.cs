1ae73e0533e84f3a0e6632345f4b85928422bc1c
Kirakees <rikhassing_33333@hotmail.com>
2016-03-30 09:53:47 +0200

    Added empty method in imgur class

--- WatchOS/WatchOS/Imgur.cs
+++ WatchOS/WatchOS/Imgur.cs
@@ -8,5 +8,56 @@ namespace WatchOS
 {
     class Imgur
     {
+        LinkedList<Post> posts;
+        LinkedListNode<Post> currentPost;
+        bool favoritesModeOn;
+
+        //Constructor
+        public Imgur()
+        {
+
+        }
+
+
+        public void loadPosts()
+        {
+
+        }
+
+        public void viewFavoritePosts()
+        {
+
+        }
+
+        public void nextPost()
+        {
+
+        }
+
+        public void previousPost()
+        {
+
+        }
+
+        public Post getCurrentPost()
+        {
+            return null;
+        }
+
+        public void upvoteCurrentPost()
+        {
+
+        }
+
+        public void downvoteCurrentPost()
+        {
+
+        }
+
+        public void favoriteCurrentPost()
+        {
+
+        }
+
     }
 }
