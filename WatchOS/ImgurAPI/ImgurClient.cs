7742c5155e98856abc7b7ca5ac59a020e8343723
Lesley Jordan van Oostenrijk <Lesley Jordan van Oostenrijk>
2016-03-30 14:52:15 +0200

    added imgur stuff + JSON

---
+++ WatchOS/ImgurAPI/ImgurClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchOS
+{
+    
+    class ImgurClient
+    {
+        private string _clientID;
+        private string _clientSecret;
+
+        public ImgurClient(string clientID, string clientSecret)
+        {
+            _clientID = clientID;
+            _clientSecret = clientSecret;
+        }
+        
+   
+    }
+}
