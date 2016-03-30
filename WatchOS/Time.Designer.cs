1c17b35a7f66a85840a39b5ff40e8d3fa77feae0
Kirakees <rikhassing_33333@hotmail.com>
2016-03-30 14:39:48 +0200

    TimeClass geupdate

--- WatchOS/Time.Designer.cs
+++ WatchOS/Time.Designer.cs
@@ -28,11 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(274, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(563, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SMART WATCH INF2I";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(151, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 135);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(509, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(327, 135);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(450, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 135);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ":";
+            // 
+            // Time
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 523);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Time";
             this.Text = "Time";
+            this.Load += new System.EventHandler(this.Time_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- WatchOS/Time.cs
+++ WatchOS/Time.cs
@@ -12,9 +12,66 @@ namespace WatchOS
 {
     public partial class Time : Form
     {
+        private int minutes;
+        private int hours;
+        private enum Mode {view, changeMinutes, changeHours};
+        private Mode currentMode;
+
         public Time()
         {
+            hours = 10;
+            minutes = 10;
             InitializeComponent();
+        }
+
+        private void Time_Load(object sender, EventArgs e)
+        {
+
+        }
+        public String getMinutes()
+        {
+            return minutes.ToString();
+        }
+        public String getHours()
+        {
+            return hours.ToString();
+        }
+        public void changeTimeMode()
+        {
+            if(currentMode == Mode.changeHours)
+            {
+                currentMode = Mode.changeMinutes;
+            }
+            else if(currentMode == Mode.changeMinutes)
+            {
+                currentMode = Mode.view;
+            }
+            else if (currentMode == Mode.view)
+            {
+                currentMode = Mode.changeHours;
+            }
+        }
+
+        public void incrementTime()
+        {
+            if(currentMode == Mode.changeHours)
+            {
+                hours++;
+            }
+            else if(currentMode == Mode.changeMinutes)
+            {
+                minutes++;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Text = hours.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Text = minutes.ToString();
         }
     }
 }
